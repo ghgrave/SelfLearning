04a7cfbbb148565dd26e72807632276a5c1bd79b
Jon-Mikel <garrisonhgrave@yahoo.com>
2021-06-21 20:59:14 -0500

    initial set up of movie poster search and resualts

--- Skwerel/client/src/App.js
+++ Skwerel/client/src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
 
+import keys from "./config/keys";
+
 function App() {
+
+  const [rows, setRows] = useState([]);
+  const performSearch = (searchTerm)=>  {
+    const url = `https://api.themoviedb.org/3`;
+    const movieRoute = `/search/movie?api_key=${keys.tmdb_api_key}&query=${searchTerm}`;
+    const endpoint = url + movieRoute;
+    fetch(endpoint)
+      .then((response) => response.json())
+      .then((searchResults) => {
+        let movies = searchResults.results
+        let movieRows = [];
+        movies.forEach((movie) => {
+          let poster = "https://image.tmdb.org/t/p/w200" + movie.poster_path
+          const movieRow = (
+            <div key={movie.id}>
+              <img src={poster} alt="poster" />
+              {movie.title}
+            </div>
+          );
+          movieRows.push(movieRow);
+        });
+        setRows(movieRows)
+      })
+      .catch(error => console.log('Error coming from API:', error));
+  }
+
+  const searchChangeHandler = (event) => {
+    console.log(event.target.value)
+    performSearch(event.target.value)
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Skwerel</h1>
+      <input
+          id="inputField"
+          type="text"
+          placeholder="enter your movie here"
+          onChange={searchChangeHandler}
+        />
+        {rows}
     </div>
   );
 }
---
+++ Skwerel/client/src/config/prod.js
@@ -0,0 +1,3 @@
+module.exports = {
+  tmdb_api_key: process.env.TMDB_API_KEY
+}
